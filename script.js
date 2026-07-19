ea4bb66c5f8ec3c5a1f03a1963301bd7e75f6cf3
Honeystarcharmie <gynendra25121977@gmail.com>
2026-07-19 23:55:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+.buy-now-button {
+    width: 100%;
+    padding: 17px;
+    margin-top: 10px;
+    border: 2px solid #eb6598;
+    border-radius: 30px;
+    background: white;
+    color: #eb6598;
+    font-size: 20px;
+    font-weight: bold;
+    transition: 0.3s;
+}
 
+.buy-now-button:hover {
+    background: #eb6598;
+    color: white;
+}
